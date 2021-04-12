bf84462a949b3e33c3f4bbbf423f1b6eb109828e
Rishabh Tiwari <r.tiwari6396@gmail.com>
2021-04-12 19:47:45 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'rt-todo-list';
+  // name='Rishabh'
+  a='Rishabh tiwari is cool!'
+  constructor(){
+//     setTimeout(()=>{
+// // this.name="Vicky"
+//     },3000);
+  }
+}
